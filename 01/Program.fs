a9d92fdaca4b2be77bba4cc81e9065bc8d63d0b4
Alessio Aurecchia <alessio@aurecchia.ch>
2021-12-01 08:33:43 +0100

    Let's try again this year

---
+++ 01/Program.fs
@@ -0,0 +1,28 @@
+﻿open System.IO
+
+let readLines (path: string) =
+    seq {
+        use reader = new StreamReader (path)
+        while not reader.EndOfStream do
+            yield reader.ReadLine ()
+    }
+
+let countOfDepthIncreases (depths: int seq) =
+    depths
+    |> Seq.pairwise
+    |> Seq.where (fun (fst, snd) -> snd > fst)
+    |> Seq.length
+
+let depths =
+    readLines "input"
+    |> Seq.map int
+
+depths
+|> countOfDepthIncreases
+|> printfn "%d"
+
+depths
+|> Seq.windowed 3
+|> Seq.map Array.sum
+|> countOfDepthIncreases
+|> printfn "%d"
